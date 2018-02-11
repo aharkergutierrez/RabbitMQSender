7b8ac5028c3b0b06d8d630261ec4418d9281d12e
Andres Harker <aharker@javeriana.edu.co>
2018-02-11 23:54:11 +0100

    Se suben los cambios del proyecto base

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # RabbitMQSender
 Este proyecto es el que envia los mensajes a una cola de mensajes en RabbitMQ
+
+Este proyecto ha sido desarrollado para ejecutar con jre 8
+Gestionando el ciclo de compilacion, empaquetado y test (No tiene JUnit implementado pero lo soportaria) con maven 4.0
+Se encuentra implementado con Spring boot y spring mvc para la exposicion de servicios rest que dejan los mensajes como string en la cola de mensajes
+Parametrizando tanto el puerto de publicacion de los servicios rest, la ip del servidor de rabbitMQ y el mombre de la cola de mensajes mediante el archivo application.properties
+El controlador rest que deja el mensaje en la cola de mensajes es MyRestController, el metodo sendMessage
+
+Una vez se levantan los servicios mediante la clase Main IniciarViaSpringBoot, se puede dejar el mensaje en la cola de mensajes mediante el consumo de la
+url http://{ip_publicacion}:{puerto_publicacion}/sendMessage?message={string_mensaje}, por ejemplo http://localhost:8080/sendMessage?message=Andres
+
+NOTA, este proyecto presupone que en la ip que se parametrice en el archivo application.properties como ip.cola.mensajes , se encuentre ejecutando un servicio de
+RabbitMQ, por otra parte tanto productos como consumidor deben apuntar a esta misma ip o nombre de maquina y a la misma cola de mensajes parametrizada en el
+campo nombre.cola.mensajes del mismo archivo.
---
+++ src/main/java/harker/gutierrez/andres/ejemplopropiospringbootmaven/MyRestController.java
@@ -0,0 +1,43 @@
+package harker.gutierrez.andres.ejemplopropiospringbootmaven;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class MyRestController {
+
+    @Value("${ip.cola.mensajes}")
+    String ipColaMensajes;
+    
+    @Value("${nombre.cola.mensajes}")
+    String nombreColaMensajes;
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Hola Springboot AHG";
+    }
+
+    @RequestMapping("/sendMessage")
+    public String sendMessage(@RequestParam("message") String message) throws IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(ipColaMensajes);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(nombreColaMensajes, false, false, false, null);
+        channel.basicPublish("", nombreColaMensajes, null, message.getBytes());
+        System.out.println(" Enviado '" + message + "'");
+        channel.close();
+        connection.close();
+
+        return "[AHG] Se coloca un mensaje en la cola de RabbitMQ y el valor es: " + message;
+    }
+
+}
